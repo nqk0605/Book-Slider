3831bca932c0a62966944dfaba1d894fe05256ef
nqk0605 <quockhang0605@gmail.com>
2024-11-13 14:40:27 +0700

    add bookslider

---
+++ .history/src/App_20241113142242.jsx
@@ -0,0 +1,26 @@
+import { Loader } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Experience } from "./components/Experience";
+import { UI } from "./components/UI";
+
+function App() {
+  return (
+    <>
+      <UI />
+      <Loader />
+      <Canvas
+        shadows
+        camera={{ position: [10, 1, 4], fov: 45 }}
+      >
+        <group position-y={0}>
+          <Suspense fallback={null}>
+            <Experience />
+          </Suspense>
+        </group>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
